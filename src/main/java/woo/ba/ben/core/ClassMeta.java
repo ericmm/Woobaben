d2e8c88b312d9631a956d88d3dccbf72f252c472
ericmm <eric0726@gmail.com>
2015-09-24 23:28:56 +1000

    small refactors

--- src/main/java/woo/ba/ben/core/ClassMeta.java
+++ src/main/java/woo/ba/ben/core/ClassMeta.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassMeta {
-    protected Class<?> clazz;
+    protected Class clazz;
     protected ClassMeta parent;
 
     private SimpleMap<String, FieldMeta> fieldMap = new SimpleArrayMap<>();
--- src/main/java/woo/ba/ben/core/FieldMeta.java
+++ src/main/java/woo/ba/ben/core/FieldMeta.java
@@ -2,7 +2,7 @@ package woo.ba.ben.core;
 
 public class FieldMeta {
     protected String name;
-    protected Class<?> type; //can be Enum, Class, Interface...
+    protected Class type; //can be Enum, Class, Interface...
     protected int modifiers; //static, transient??, instance,
     protected long offset;
 
