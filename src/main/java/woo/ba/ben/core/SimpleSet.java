347df5d11d994cc1791df9f4ab9573eeaf906960
ericmm <eric0726@gmail.com>
2015-10-03 12:46:02 +1000

    update typo in test

---
+++ src/main/java/woo/ba/ben/core/SimpleSet.java
@@ -0,0 +1,6 @@
+package woo.ba.ben.core;
+
+
+public interface SimpleSet<E> {
+
+}
--- src/test/java/woo/ba/ben/core/SimpleArrayMapTest.java
+++ src/test/java/woo/ba/ben/core/SimpleArrayMapTest.java
@@ -49,59 +49,61 @@ public class SimpleArrayMapTest {
     }
 
     @Test
-    public void testPerformance() {
+    public void testPerformance() throws InterruptedException {
         int size = 3000000;
-        HashMap hashMap = new HashMap(size * 2);
-        SimpleMap simpleMap = new SimpleArrayMap<>(size * 2);
 
-        //warm up
-        for (int i = 0; i < 100; i++) {
-            final String obj = String.valueOf(i);
-            simpleMap.put(obj, obj);
-            simpleMap.remove(obj);
+        for (int times = 0; times<10; times++) {
+            HashMap hashMap = new HashMap(size * 2);
+            SimpleMap simpleMap = new SimpleArrayMap<>(size * 2, 0.75f);
 
-            hashMap.put(obj, obj);
-            hashMap.remove(obj);
+            //warm up
+            for (int i = 0; i < 100; i++) {
+                final String obj = String.valueOf(i);
+                simpleMap.put(obj, obj);
+                simpleMap.remove(obj);
+
+                hashMap.put(obj, obj);
+                hashMap.remove(obj);
+            }
+
+            final long start = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                hashMap.put("int", String.valueOf(i));
+                hashMap.put(String.valueOf(i), "int");
+            }
+            final long end = System.currentTimeMillis();
+            System.out.println("HashMap add used :" + (end - start));
+
+            System.gc();
+
+
+            final long start1 = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                simpleMap.put("int", String.valueOf(i));
+                simpleMap.put(String.valueOf(i), "int");
+            }
+            final long end1 = System.currentTimeMillis();
+            System.out.println("SimpleMap add used :" + (end1 - start1));
+
+            System.gc();
+
+            final long start2 = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                hashMap.remove("int");
+                hashMap.remove(String.valueOf(i));
+            }
+            final long end2 = System.currentTimeMillis();
+            System.out.println("HashMap remove used :" + (end2 - start2));
+
+            System.gc();
+
+            final long start3 = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                simpleMap.remove("int");
+                simpleMap.remove(String.valueOf(i));
+            }
+            final long end3 = System.currentTimeMillis();
+            System.out.println("SimpleMap remove used :" + (end3 - start3));
         }
-
-        System.gc();
-
-        final long start = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            hashMap.put("int", String.valueOf(i));
-            hashMap.put(String.valueOf(i), "int");
-        }
-        final long end = System.currentTimeMillis();
-        System.out.println("HashMap add used :" + (end - start));
-
-        System.gc();
-
-        final long start1 = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            simpleMap.put("int", String.valueOf(i));
-            simpleMap.put(String.valueOf(i), "int");
-        }
-        final long end1 = System.currentTimeMillis();
-        System.out.println("SimpleMap add used :" + (end1 - start1));
-
-        System.gc();
-
-        final long start2 = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            hashMap.remove("int");
-            hashMap.remove(String.valueOf(i));
-        }
-        final long end2 = System.currentTimeMillis();
-        System.out.println("HashMap remove used :" + (end2 - start2));
-
-        System.gc();
-
-        final long start3 = System.currentTimeMillis();
-        for (int i = 0; i < size; i++) {
-            simpleMap.remove("int");
-            simpleMap.remove(String.valueOf(i));
-        }
-        final long end3 = System.currentTimeMillis();
-        System.out.println("SimpleMap add used :" + (end3 - start3));
     }
 }
